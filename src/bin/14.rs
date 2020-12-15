043eed917f0ab1c3c3def6afc97c80fb274818c0
Jackson Riley <jackson.riley@btinternet.com>
2020-12-15 19:53:55 +0000

    Feel like this should be faster than it is

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::time::Instant;
 #[macro_use]
 extern crate serde_scan;
@@ -20,6 +20,7 @@ fn main() -> Result<(), std::io::Error> {
     let now = Instant::now();
     let input = std::fs::read_to_string("input/14")?;
     let commands = parse_input(&input);
+    println!("Time: {}µs", now.elapsed().as_micros());
     println!("Part 1: {}", part_one(&commands));
     println!("Time: {}µs", now.elapsed().as_micros());
     println!("Part 2: {}", part_two(&commands));
@@ -72,32 +73,44 @@ fn masked1(mut value: u64, mask: &HashMap<usize, MaskVal>) -> u64 {
     value
 }
 
-fn masked2(mut value: u64, mask: &HashMap<usize, MaskVal>) -> Vec<u64> {
-    // First, do all of the MaskVal::One bit sets, as these can all be done on one value.
-    // Also set all of the MashVal::X bits to 1.
+fn masked2(mut value: u64, masks: &(u64, Vec<u64>)) -> Vec<u64> {
+    let one_mask = &masks.0;
+    let xs_masks = &masks.1;
     let mut addresses = Vec::new();
-    for (k, v) in mask.iter() {
-        match v {
-            MaskVal::One | MaskVal::X => {
-                // Set that bit to 1
-                value |= 1 << k
-            }
-            _ => {}
-        }
-    }
+
+    // Apply the one mask first, as this only has to be done once.
+    value |= one_mask;
+
     // Next do the floating bit stuff - for each address in the vector, add
     // one with the X bit switched to 0.
     addresses.push(value);
-    for (k, v) in mask.iter() {
+    for x_mask in xs_masks.iter() {
         let mut new_addresses = Vec::new();
-        if let MaskVal::X = v {
-            for addr in addresses.iter() {
-                new_addresses.push(addr & !(1 << k));
-            }
+        for addr in addresses.iter() {
+            new_addresses.push(addr & x_mask);
         }
         addresses.extend(new_addresses.into_iter());
     }
     addresses
+}
+
+fn calculate_masks(mask: &HashMap<usize, MaskVal>) -> (u64, Vec<u64>) {
+    // Return
+    //  * a u64 with ones where 1 and X are in the mask
+    //  * a vector of u64s with all ones except for the X position, for all X's
+    let mut one_mask = 0u64;
+    let mut xs_masks: Vec<u64> = Vec::new();
+    for (k, v) in mask.iter() {
+        match v {
+            MaskVal::One => one_mask |= 1 << k,
+            MaskVal::X => {
+                one_mask |= 1 << k;
+                xs_masks.push(!(1 << k))
+            }
+            _ => {}
+        }
+    }
+    (one_mask, xs_masks)
 }
 
 fn part_one(commands: &[Command]) -> u64 {
@@ -116,14 +129,24 @@ fn part_one(commands: &[Command]) -> u64 {
 
 fn part_two(commands: &[Command]) -> u64 {
     let mut memory: HashMap<u64, u64> = HashMap::new();
-    let mut curr_mask = HashMap::new();
+    let mut curr_masks: (u64, Vec<u64>) = (0, Vec::new());
     for command in commands.iter() {
         match command {
-            Command::Mask(mask) => curr_mask = mask.clone(),
+            Command::Mask(mask) => {
+                curr_masks = calculate_masks(mask);
+            }
             Command::Mem((addr, val)) => {
-                for masked_addr in masked2(*addr, &curr_mask).iter() {
+                let now = Instant::now();
+                for masked_addr in masked2(*addr, &curr_masks).iter() {
                     memory.insert(*masked_addr, *val);
                 }
+                println!(
+                    "Mask time: {}µs\n Masks {:?}\n Addr {}\n Val {}\n",
+                    now.elapsed().as_micros(),
+                    curr_masks,
+                    addr,
+                    val
+                );
             }
         }
     }
