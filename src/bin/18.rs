f8f9221f7ee1b0d60410457647ac7c3e8661c361
Jackson Riley <jackson.riley@btinternet.com>
2020-12-19 10:03:54 +0000

    Day 18 works - don't look at it if you like your eyes

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -3,17 +3,123 @@ use std::time::Instant;
 
 type Expression = VecDeque<Token>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Token {
     LB,
     RB,
     Plus,
     Multiply,
-    Number(u32),
+    Number(u64),
+}
+use Token::*;
+
+fn calculate(expression: &mut Expression) -> u64 {
+    // Basic idea is that we recursively calculate the expression from left to
+    // right, calling the function again for every left bracket we meet, and
+    // returning at every right bracket or when the expression is finished.
+    let mut acc = 0;
+    while !expression.is_empty() {
+        let tok = expression.pop_front().unwrap();
+        match tok {
+            RB => break,
+            Number(n) => {
+                let op = expression.pop_front().unwrap();
+                let b = expression.pop_front().unwrap();
+                match b {
+                    Number(m) => match op {
+                        Plus => acc += n + m,
+                        Multiply => acc += n * m,
+                        _ => panic!("Didn't expect 'op' {:?}", op),
+                    },
+                    LB => {
+                        let right = calculate(expression);
+                        match op {
+                            Plus => acc += n + right,
+                            Multiply => acc += n * right,
+                            _ => panic!("Didn't expect 'op' {:?}", op),
+                        }
+                    }
+                    _ => panic!("Didn't expect b {:?}", b),
+                }
+            }
+            LB => acc = calculate(expression),
+            Plus => {
+                let right = expression.pop_front().unwrap();
+                match right {
+                    Number(m) => acc += m,
+                    LB => acc += calculate(expression),
+                    _ => panic!("Didn't expect right {:?}", right),
+                }
+            }
+            Multiply => {
+                let right = expression.pop_front().unwrap();
+                match right {
+                    Number(m) => acc *= m,
+                    LB => acc *= calculate(expression),
+                    _ => panic!("Didn't expect right {:?}", right),
+                }
+            }
+        }
+    }
+    acc
 }
 
-fn calculate(expression: &mut Expression) -> u32 {
-    
+fn calculate2(expression: &mut Expression, from_mul: bool) -> u64 {
+    // Differences from the first part:
+    //  * when we meet a multiply, we call the function so as to effectively
+    //    have a lower precedence. This is so non-extensible it's not even
+    //    funny.
+    //  * pass in to each function call whether or not we've come from a
+    //    multiply or a left bracket - we don't want to pop right brackets if
+    //    we've come from a multiply.
+    let mut acc = 0;
+    while !expression.is_empty() {
+        let tok = expression.pop_front().unwrap();
+        match tok {
+            RB => {
+                if from_mul {
+                    // Don't want to actually pop the RB in this case
+                    expression.push_front(RB)
+                }
+                break;
+            }
+            Number(n) => {
+                let op = expression.pop_front();
+                match op {
+                    Some(Plus) => {
+                        let b = expression.pop_front().unwrap();
+                        match b {
+                            Number(m) => acc += n + m,
+                            LB => acc += n + calculate2(expression, false),
+                            _ => panic!("Didn't expect b {:?}", b),
+                        }
+                    }
+                    Some(Multiply) => {
+                        acc += n * calculate2(expression, true);
+                    }
+                    Some(RB) => {
+                        if from_mul {
+                            expression.push_front(RB);
+                            return n;
+                        }
+                    }
+                    None => return n,
+                    _ => panic!("Didn't expect 'op' {:?}", op),
+                }
+            }
+            LB => acc = calculate2(expression, false),
+            Plus => {
+                let right = expression.pop_front().unwrap();
+                match right {
+                    Number(m) => acc += m,
+                    LB => acc += calculate2(expression, false),
+                    _ => panic!("Didn't expect right {:?}", right),
+                }
+            }
+            Multiply => acc *= calculate2(expression, true),
+        }
+    }
+    acc
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -21,7 +127,7 @@ fn main() -> Result<(), std::io::Error> {
     let input = std::fs::read_to_string("input/18")?;
     let expressions = parse_input(&input);
     println!("Part 1: {}", part_one(&expressions));
-    // println!("Part 2: {}", part_two(&all_info));
+    println!("Part 2: {}", part_two(&expressions));
     println!("Time: {}µs", now.elapsed().as_micros());
     Ok(())
 }
@@ -37,7 +143,7 @@ fn parse_input(input: &str) -> Vec<Expression> {
                     '+' => Some(Token::Plus),
                     '*' => Some(Token::Multiply),
                     maybe_num => match maybe_num.to_digit(10) {
-                        Some(n) => Some(Token::Number(n)),
+                        Some(n) => Some(Token::Number(n as u64)),
                         None => None,
                     },
                 })
@@ -47,39 +153,45 @@ fn parse_input(input: &str) -> Vec<Expression> {
     expressions
 }
 
-fn part_one(expressions: &[Expression]) -> u32 {
-    println!("{:?}", &expressions[..3]);
-    expressions.iter().map(|e| calculate(e.clone())).sum()
+fn part_one(expressions: &[Expression]) -> u64 {
+    expressions
+        .iter()
+        .map(|e| {
+            let mut c = e.clone();
+            calculate(&mut c)
+        })
+        .sum()
+}
+
+fn part_two(expressions: &[Expression]) -> u64 {
+    expressions
+        .iter()
+        .map(|e| {
+            let mut c = e.clone();
+            calculate2(&mut c, false)
+        })
+        .sum()
 }
 
 #[test]
 fn test_examples() {
-    let input = "class: 1-3 or 5-7
-row: 6-11 or 33-44
-seat: 13-40 or 45-50
+    let input = "2 * 3 + (4 * 5)";
+    let expressions = parse_input(&input);
+    assert_eq!(part_one(&expressions), 26);
+    assert_eq!(part_two(&expressions), 46);
 
-your ticket:
-7,1,14
+    let input = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+    let expressions = parse_input(&input);
+    assert_eq!(part_one(&expressions), 437);
+    assert_eq!(part_two(&expressions), 1445);
 
-nearby tickets:
-7,3,47
-40,4,50
-55,2,20
-38,6,12";
-    let all_info = parse_input(&input);
-    assert_eq!(part_one(&all_info), 71);
+    let input = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+    let expressions = parse_input(&input);
+    assert_eq!(part_one(&expressions), 12240);
+    assert_eq!(part_two(&expressions), 669060);
 
-    let input = "class: 0-1 or 4-19
-departure row: 0-5 or 8-19
-seat: 0-13 or 16-19
-
-your ticket:
-11,12,13
-
-nearby tickets:
-3,9,18
-15,1,5
-5,14,9";
-    let all_info = parse_input(&input);
-    assert_eq!(part_two(&all_info), 11);
+    let input = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+    let expressions = parse_input(&input);
+    assert_eq!(part_one(&expressions), 13632);
+    assert_eq!(part_two(&expressions), 23340);
 }
