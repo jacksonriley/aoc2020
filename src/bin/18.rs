d401e7f8dfbff29fc792ac72459201c180ee9701
Jackson Riley <jackson.riley@btinternet.com>
2020-12-19 11:37:21 +0000

    Convert day 18 to shunting-yard algorithm and reverse polish notation

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,125 +1,81 @@
+use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::time::Instant;
 
 type Expression = VecDeque<Token>;
 
-#[derive(Debug, Clone)]
-enum Token {
-    LB,
-    RB,
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum OpType {
     Plus,
     Multiply,
+    LB,
+    RB,
+}
+use OpType::*;
+
+#[derive(Debug, Clone)]
+enum Token {
+    Op(OpType),
     Number(u64),
 }
 use Token::*;
 
-fn calculate(expression: &mut Expression) -> u64 {
-    // Basic idea is that we recursively calculate the expression from left to
-    // right, calling the function again for every left bracket we meet, and
-    // returning at every right bracket or when the expression is finished.
-    let mut acc = 0;
+fn calculate_rpn(mut expression: Expression, op_precedence: HashMap<OpType, u8>) -> VecDeque<Token> {
+    // Use https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+    let mut output_queue: VecDeque<Token> = VecDeque::new();
+    let mut op_stack: Vec<OpType> = Vec::new();
+
     while !expression.is_empty() {
         let tok = expression.pop_front().unwrap();
         match tok {
-            RB => break,
-            Number(n) => {
-                let op = expression.pop_front().unwrap();
-                let b = expression.pop_front().unwrap();
-                match b {
-                    Number(m) => match op {
-                        Plus => acc += n + m,
-                        Multiply => acc += n * m,
-                        _ => panic!("Didn't expect 'op' {:?}", op),
-                    },
-                    LB => {
-                        let right = calculate(expression);
-                        match op {
-                            Plus => acc += n + right,
-                            Multiply => acc += n * right,
-                            _ => panic!("Didn't expect 'op' {:?}", op),
+            Number(n) => output_queue.push_back(Number(n)),
+            Op(op) => {
+                match op {
+                    LB => op_stack.push(LB),
+                    RB => {
+                        while op_stack.last().unwrap() != &LB {
+                            output_queue.push_back(Op(op_stack.pop().unwrap()));
                         }
+                        // Discard the LB
+                        op_stack.pop();
                     }
-                    _ => panic!("Didn't expect b {:?}", b),
-                }
-            }
-            LB => acc = calculate(expression),
-            Plus => {
-                let right = expression.pop_front().unwrap();
-                match right {
-                    Number(m) => acc += m,
-                    LB => acc += calculate(expression),
-                    _ => panic!("Didn't expect right {:?}", right),
-                }
-            }
-            Multiply => {
-                let right = expression.pop_front().unwrap();
-                match right {
-                    Number(m) => acc *= m,
-                    LB => acc *= calculate(expression),
-                    _ => panic!("Didn't expect right {:?}", right),
+                    _ => {
+                        while !op_stack.is_empty()
+                            && op_stack.last().unwrap() != &LB
+                            && op_precedence.get(&op) <= op_precedence.get(op_stack.last().unwrap())
+                        {
+                            output_queue.push_back(Op(op_stack.pop().unwrap()));
+                        }
+                        op_stack.push(op);
+                    }
                 }
             }
         }
     }
-    acc
+    while !op_stack.is_empty() {
+        output_queue.push_back(Op(op_stack.pop().unwrap()));
+    }
+    output_queue
 }
 
-fn calculate2(expression: &mut Expression, from_mul: bool) -> u64 {
-    // Differences from the first part:
-    //  * when we meet a multiply, we call the function so as to effectively
-    //    have a lower precedence. This is so non-extensible it's not even
-    //    funny.
-    //  * pass in to each function call whether or not we've come from a
-    //    multiply or a left bracket - we don't want to pop right brackets if
-    //    we've come from a multiply.
-    let mut acc = 0;
-    while !expression.is_empty() {
-        let tok = expression.pop_front().unwrap();
+fn evaluate_rpn(mut tokens: VecDeque<Token>) -> u64 {
+    let mut stack : Vec<u64> = Vec::new();
+    while !tokens.is_empty() {
+        let tok = tokens.pop_front().unwrap();
         match tok {
-            RB => {
-                if from_mul {
-                    // Don't want to actually pop the RB in this case
-                    expression.push_front(RB)
-                }
-                break;
-            }
-            Number(n) => {
-                let op = expression.pop_front();
+            Number(n) => stack.push(n),
+            Op(op) => {
+                let second = stack.pop().unwrap();
+                let first = stack.pop().unwrap();
                 match op {
-                    Some(Plus) => {
-                        let b = expression.pop_front().unwrap();
-                        match b {
-                            Number(m) => acc += n + m,
-                            LB => acc += n + calculate2(expression, false),
-                            _ => panic!("Didn't expect b {:?}", b),
-                        }
-                    }
-                    Some(Multiply) => {
-                        acc += n * calculate2(expression, true);
-                    }
-                    Some(RB) => {
-                        if from_mul {
-                            expression.push_front(RB);
-                            return n;
-                        }
-                    }
-                    None => return n,
-                    _ => panic!("Didn't expect 'op' {:?}", op),
+                    Plus => stack.push (first + second),
+                    Multiply => stack.push(first * second),
+                    _ => panic!("Didn't exprect op {:?} in RPN!", op),
                 }
             }
-            LB => acc = calculate2(expression, false),
-            Plus => {
-                let right = expression.pop_front().unwrap();
-                match right {
-                    Number(m) => acc += m,
-                    LB => acc += calculate2(expression, false),
-                    _ => panic!("Didn't expect right {:?}", right),
-                }
-            }
-            Multiply => acc *= calculate2(expression, true),
         }
     }
-    acc
+    stack.pop().unwrap()
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -138,10 +94,10 @@ fn parse_input(input: &str) -> Vec<Expression> {
         expressions.push(
             line.chars()
                 .filter_map(|c| match c {
-                    '(' => Some(Token::LB),
-                    ')' => Some(Token::RB),
-                    '+' => Some(Token::Plus),
-                    '*' => Some(Token::Multiply),
+                    '(' => Some(Token::Op(OpType::LB)),
+                    ')' => Some(Token::Op(OpType::RB)),
+                    '+' => Some(Token::Op(OpType::Plus)),
+                    '*' => Some(Token::Op(OpType::Multiply)),
                     maybe_num => match maybe_num.to_digit(10) {
                         Some(n) => Some(Token::Number(n as u64)),
                         None => None,
@@ -157,8 +113,10 @@ fn part_one(expressions: &[Expression]) -> u64 {
     expressions
         .iter()
         .map(|e| {
-            let mut c = e.clone();
-            calculate(&mut c)
+            let prec = vec![(Plus, 0u8), (Multiply, 0u8)];
+            let op_prec: HashMap<OpType, u8> = prec.into_iter().collect();
+            let rpn = calculate_rpn(e.clone(), op_prec);
+            evaluate_rpn(rpn)
         })
         .sum()
 }
@@ -167,8 +125,10 @@ fn part_two(expressions: &[Expression]) -> u64 {
     expressions
         .iter()
         .map(|e| {
-            let mut c = e.clone();
-            calculate2(&mut c, false)
+            let prec = vec![(Plus, 1u8), (Multiply, 0u8)];
+            let op_prec: HashMap<OpType, u8> = prec.into_iter().collect();
+            let rpn = calculate_rpn(e.clone(), op_prec);
+            evaluate_rpn(rpn)
         })
         .sum()
 }
