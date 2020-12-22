9ed9d494892311d890a0eb32e04aeff889d2773a
Jackson Riley <jackson.riley@btinternet.com>
2020-12-22 21:04:47 +0000

    Remove repeated code

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -26,23 +26,23 @@ fn play_simple_round(decks: &mut (Deck, Deck)) {
     }
 }
 
-fn play_recursive_subgame(mut decks: (Deck, Deck)) -> Winner {
+fn play_recursive_game(mut decks: (Deck, Deck)) -> (Winner, Deck) {
     let mut seen: HashSet<(Deck, Deck)> = HashSet::new();
 
     while !decks.0.is_empty() && !decks.1.is_empty() {
         if !seen.insert((decks.0.clone(), decks.1.clone())) {
             // We've already seen these decks in a previous round - player 1
             // wins
-            return Winner::Player1;
+            return (Winner::Player1, decks.0)
         }
         let top_0 = decks.0.pop_front().unwrap();
         let top_1 = decks.1.pop_front().unwrap();
         if decks.0.len() >= top_0 && decks.1.len() >= top_1 {
             // Determine the winner by playing a sub-game
-            match play_recursive_subgame((
+            match play_recursive_game((
                 decks.0.iter().take(top_0).cloned().collect(),
                 decks.1.iter().take(top_1).cloned().collect(),
-            )) {
+            )).0 {
                 Winner::Player1 => {
                     decks.0.push_back(top_0);
                     decks.0.push_back(top_1);
@@ -69,60 +69,12 @@ fn play_recursive_subgame(mut decks: (Deck, Deck)) -> Winner {
     }
 
     if decks.0.is_empty() {
-        Winner::Player2
+        (Winner::Player2, decks.1)
     } else {
-        Winner::Player1
+        (Winner::Player1, decks.0)
     }
 }
 
-fn play_recursive_game(mut decks: (Deck, Deck)) -> Deck {
-    let mut seen: HashSet<(Deck, Deck)> = HashSet::new();
-
-    while !decks.0.is_empty() && !decks.1.is_empty() {
-        if !seen.insert((decks.0.clone(), decks.1.clone())) {
-            // We've already seen these decks in a previous round - player 1
-            // wins
-            return decks.0;
-        }
-        let top_0 = decks.0.pop_front().unwrap();
-        let top_1 = decks.1.pop_front().unwrap();
-        if decks.0.len() >= top_0 && decks.1.len() >= top_1 {
-            // Determine the winner by playing a sub-game
-            match play_recursive_subgame((
-                decks.0.iter().take(top_0).cloned().collect(),
-                decks.1.iter().take(top_1).cloned().collect(),
-            )) {
-                Winner::Player1 => {
-                    decks.0.push_back(top_0);
-                    decks.0.push_back(top_1);
-                }
-                Winner::Player2 => {
-                    decks.1.push_back(top_1);
-                    decks.1.push_back(top_0);
-                }
-            }
-        } else {
-            // The higher value card wins
-            match top_0.cmp(&top_1) {
-                Ordering::Greater => {
-                    decks.0.push_back(top_0);
-                    decks.0.push_back(top_1);
-                }
-                Ordering::Less => {
-                    decks.1.push_back(top_1);
-                    decks.1.push_back(top_0);
-                }
-                Ordering::Equal => panic!("Ties should not occur"),
-            }
-        }
-    }
-
-    if decks.0.is_empty() {
-        decks.1
-    } else {
-        decks.0
-    }
-}
 
 fn main() -> Result<(), std::io::Error> {
     let now = Instant::now();
@@ -172,7 +124,7 @@ fn part_one(decks: &(Deck, Deck)) -> usize {
 
 fn part_two(decks: &(Deck, Deck)) -> usize {
     let decks = decks.clone();
-    let winner = play_recursive_game(decks);
+    let winner = play_recursive_game(decks).1;
     winner
         .iter()
         .enumerate()
